330086567e6d55b5faaf930274d6f60b6fd5df10
yos10 <leonidmeteorswarm-git@yahoo.co.jp>
2021-02-12 19:04:08 +0900

    WebSocket の接続時と切断時にログを表示

--- app/entry.js
+++ app/entry.js
@@ -3,14 +3,14 @@ import $ from 'jquery';
 const block = $('#block');
 const scalingButton = $('#scaling-button');
 
-scalingButton.click(() => {
+scalingButton.on('click', () => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
 });
 
 const movingButton = $('#moving-button');
 
-movingButton.click(() => {
+movingButton.on('click', () => {
   block.animate({ 'marginLeft': '500px' }, 500);
   block.animate({ 'marginLeft': '20px' }, 1000);
 });
@@ -22,3 +22,5 @@ const socket = io('http://localhost:8000');
 socket.on('server-status', (data) => {
   loadavg.text(data.loadavg.toString());
 });
+socket.on('connect', () => { console.log('接続しました'); });
+socket.on('disconnect', () => { console.log('切断しました'); });
